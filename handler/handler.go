8f7f01e4ca4fe5fef092ac21e1cee2f7090e6bbc
Walter Alípio da Silva <walteralipio@outlook.com>
2026-09-02 22:41:01 -0300

    feat: add createOpening handler logic

--- handler/handler.go
+++ handler/handler.go
@@ -1,1 +1,16 @@
 package handler
+
+import (
+	config "github.com/Walter-Alipio/gopportunities.git/Config"
+	"gorm.io/gorm"
+)
+
+var (
+	logger *config.Logger
+	db     *gorm.DB
+)
+
+func InitializeHandler() {
+	logger = config.GetLogger("handler")
+	db = config.GetSQLite()
+}
--- main.go
+++ main.go
@@ -22,4 +22,4 @@ func main() {
 	router.Initialize()
 }
 
-// 2:10:55
+// 2:50:08
