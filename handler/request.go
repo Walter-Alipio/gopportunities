2267a4ae20884650fc087be2030d8e91be77e36c
Walter Alípio da Silva <walteralipio@outlook.com>
2026-09-13 11:31:36 -0300

    feat add update logic

--- handler/request.go
+++ handler/request.go
@@ -6,6 +6,8 @@ func errParamIsRequired(name, typ string) error {
 	return fmt.Errorf("param: %s (type: %s) is required", name, typ)
 }
 
+// todo
+// extract interface to requests
 type CreateOpeningRequest struct {
 	Role     string `json:"role"`
 	Company  string `json:"company"`
@@ -39,4 +41,24 @@ func (r *CreateOpeningRequest) Validate() error {
 	}
 
 	return nil
+}
+
+// updateOpening
+type UpdateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (r *UpdateOpeningRequest) Validate() error {
+	// if any field was provided, validation is truthy
+	if r.Role != "" || r.Company != "" || r.Location != "" || r.Remote != nil || r.Link != "" || r.Salary > 0 {
+		return nil
+	}
+	// id none of the fields were provided, validation fails
+	return fmt.Errorf("at least one field must be provided")
+
 }
