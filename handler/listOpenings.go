78f6721da7060c669abcf12c54bbcd9fa8656735
Walter Alípio da Silva <walteralipio@outlook.com>
2026-09-14 20:51:14 -0300

    docs: add documentation for list all openings

--- handler/listOpenings.go
+++ handler/listOpenings.go
@@ -7,6 +7,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// @BasePath /api/v1
+
+// @Summary List opening
+// @Description List all job openings
+// @Tags Openings
+// @Accept json
+// @Produce json
+// @Success 200 {object} ListOpeningsResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /opening [get]
 func ListOpeningHandler(ctx *gin.Context) {
 	openings := []schemas.Opening{}
 	if err := db.Find(&openings).Error; err != nil {
