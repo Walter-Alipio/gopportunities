383201e917ee596f8230cb7506a4453019295691
Walter Alípio da Silva <walteralipio@outlook.com>
2026-09-14 21:27:48 -0300

    update: convert id to int before db query to prevent sql injection

--- handler/deleteOpening.go
+++ handler/deleteOpening.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/Walter-Alipio/gopportunities/schemas"
 	"github.com/gin-gonic/gin"
@@ -28,8 +29,14 @@ func DeleteOpeningHandler(ctx *gin.Context) {
 		return
 	}
 
+	parsedId, err := strconv.Atoi(id)
+	if err != nil {
+		sendError(ctx, http.StatusBadRequest,
+			errParamIsRequired("id", "queryParameter").Error())
+	}
+
 	opening := schemas.Opening{}
-	if err := db.First(&opening, id).Error; err != nil {
+	if err := db.First(&opening, parsedId).Error; err != nil {
 		sendError(ctx, http.StatusNotFound,
 			fmt.Sprintf("opening with id: %s not found", id))
 		return
--- handler/updateOpening.go
+++ handler/updateOpening.go
@@ -3,6 +3,7 @@ package handler
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/Walter-Alipio/gopportunities/schemas"
 	"github.com/gin-gonic/gin"
@@ -40,8 +41,14 @@ func UpdateOpeningHandler(ctx *gin.Context) {
 		return
 	}
 
+	parsedId, err := strconv.Atoi(id)
+	if err != nil {
+		sendError(ctx, http.StatusBadRequest,
+			errParamIsRequired("id", "queryParameter").Error())
+	}
+
 	opening := schemas.Opening{}
-	if err := db.First(&opening, id).Error; err != nil {
+	if err := db.First(&opening, parsedId).Error; err != nil {
 		sendError(ctx, http.StatusNotFound,
 			fmt.Sprintf("opening with id: %s not found", id))
 		return
